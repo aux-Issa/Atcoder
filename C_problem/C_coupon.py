09910bac6ef1c8d9f33f2178c50289b41a57af94
aux-Issa <issa1116184028@gmail.com>
2022-04-28 01:32:25 +0900

    coupon

--- C_problem/C_coupon.py
+++ C_problem/C_coupon.py
@@ -1,15 +1,17 @@
 # https://atcoder.jp/contests/abc246/tasks/abc246_c
+# 答え: https://qiita.com/u2dayo/items/4896cea07b59c707df3a#c%E5%95%8F%E9%A1%8Ccoupon
 # review is reqiured
 N, K, X = map(int, input().split())
 A = list(map(int, input().split()))
 
 ans = sum(A)
-remain_k = K
-comsumption_one_cool = sum(x //X for x in A)
-remain_price = sorted([x % X for x in A], reverse=True)
+# 以下の二つの大小
+remained_k = K
+consumption_one_cool = sum([a // X for a in A])
 
-ans -= X*min(comsumption_one_cool, remain_k)
-remain_k -= min(comsumption_one_cool, remain_k)
-ans -= sum(remain_price[:remain_k])
+remained_price = sorted([ a % X for a in A], reverse=True)
+
+ans -= X*min(remained_k, consumption_one_cool)
+remained_k -= min(remained_k, consumption_one_cool)
+ans -= sum(remained_price[:remained_k])
 print(ans)
-
